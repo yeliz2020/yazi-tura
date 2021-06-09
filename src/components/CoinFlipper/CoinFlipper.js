887186e1b84cf75a2be82c29628033663165c567
Yeliz İşler <yeliz.isler@gmail.com>
2021-06-09 23:58:14 +0300

    states added

--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -10,9 +10,25 @@ class CoinFlipper extends Component {
     this.state = {
       side: "tura",
       flipping: false,
+      counter: 0, //atış sayısını tutan sayaç
+      headCounter: 0, //tura sayısını tutan sayaç
+      tailCounter: 0, // yazı sayısını tutan sayaç
     };
+    this.handleClick = this.handleClick.bind(this);
   }
   handleClick = () => {
+    //olasılıkları tutan bir array yazıp, random fonk.ile seçileni rand dedğişkenine atayalım
+    let possibilities = ["yazi", "tura"];
+    let rand = possibilities[Math.floor(Math.random() * possibilities.length)];
+
+    //random fonk. ile rand değişkenine atanan değere göre ilgili state'in sayaç değerini setState ile güncelleyelim
+    rand === "yazi"
+      ? this.setState({ tailCounter: this.state.tailCounter + 1 })
+      : this.setState({ headCounter: this.state.headCounter + 1 });
+    //side state'i rand değişkeninin değerine göre "yazi" veya "tura" olarak güncellenir
+    this.setState({ side: rand });
+    //her tıklamayla atış değerini artırarak state güncellenir
+    this.setState({ counter: this.state.counter + 1 });
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
     this.setState({ flipping: true });
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
@@ -22,16 +38,14 @@ class CoinFlipper extends Component {
   render() {
     return (
       <div className="CoinFlipper">
-        <h1>Yazı mı Tura mı?</h1>
+        <h1> Yazı mı Tura mı ? </h1>
         <Coin side={this.state.side} flipping={this.state.flipping} />
-        <button onClick={this.handleClick}>At!</button>
+        <button onClick={this.handleClick}> At! </button>
         <p>
-          Toplam
-          <strong> 5 </strong>
-          atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
-          si yazı geldi.
+          Toplam <strong> {this.state.counter} </strong>
+          atıştan<br /> <strong> {this.state.headCounter} </strong>
+          tura<br /> <strong> {this.state.tailCounter} </strong>
+          yazı
         </p>
       </div>
     );
